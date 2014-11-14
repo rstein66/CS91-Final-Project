97c7e4f4d1dd45e048e5a41e6473d9257d0df3ef
Brooke Kelsey <bkelsey1@cs.swarthmore.edu>
2014-11-14 13:45:11 -0500

    buttons

--- trivia/views.py
+++ trivia/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.template import RequestContext, loader
 from models import Flashcard
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World, you are at the trivia index.")
+	latest_question_list = Flashcard.objects.all()
+	template = loader.get_template('trivia/index.html')
+	context = RequestContext(request, {'latest_question_list':latest_question_list})
+	return HttpResponse(template.render(context))
+	#return HttpResponse("Hello World, you are at the trivia index.")
 
 def FCdetail(request, q_id):
 	currQ = get_object_or_404(Flashcard, q_id = q_id)
